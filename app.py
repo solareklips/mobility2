8f1679f90a0b4aa97c7ac91f3edec3f4b464662d
solareklips <117949030+solareklips@users.noreply.github.com>
2023-09-20 19:16:07 +0000

    opdateret bogmærke

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -15,8 +15,13 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
-# NÅET TIL 3:14:21 - SKAL TIL RENDER.COM
-# https://youtu.be/yBDHkveJUf4?si=yME9vxHUskpbFbLT&t=11661
+# NÅET TIL 3:27:33 - SKAL TIL RENDER.COM
+# https://youtu.be/yBDHkveJUf4?si=hTzRSp2RQYrdrLSV&t=12453
--- database.py
+++ database.py
@@ -13,8 +13,20 @@ engine = create_engine(
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
+    result = conn.execute(text("SELECT * FROM jobs"))
     jobs = []
     for row in result.all():
       jobs.append(row._mapping)
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      val=id
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
