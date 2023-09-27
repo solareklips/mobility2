469cd2841e7055367f81cc426be484133de4f5dd
solareklips <117949030+solareklips@users.noreply.github.com>
2023-09-27 19:47:37 +0000

    started jobpage

--- app.py
+++ app.py
@@ -18,10 +18,11 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
-  return jsonify(job)
+  return render_template('jobpage.html', 
+                        job=job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
-# NÅET TIL 3:27:33 - SKAL TIL RENDER.COM
-# https://youtu.be/yBDHkveJUf4?si=hTzRSp2RQYrdrLSV&t=12453
+# NÅET TIL 3:30:59 - SKAL TIL RENDER.COM
+# https://youtu.be/yBDHkveJUf4?si=x_GcKG_gCxIP0WVP&t=12659
